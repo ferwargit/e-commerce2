a4d4bef9e5927b8d1cb6db950783d1f78ae60cdb
fernando <ferwar@hotmail.com>
2024-11-13 19:47:39 -0300

    ajuste formulario de contacto

---
+++ api/contacto.js
@@ -0,0 +1,5 @@
+setTimeout(function () {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+}, 50);
